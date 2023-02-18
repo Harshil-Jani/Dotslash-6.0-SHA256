186c59715a7571907bc18da798f3fc44aa268f22
ayush572 <ayushdagaeinstein@gmail.com>
2023-02-18 18:25:13 +0530

    Harshil Pushing from Ayush Laptop
    Signed-off-by: ayush572 <ayushdagaeinstein@gmail.com>

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork : 'polygon_testnet',
+    networks:{
+      hardhat: {},
+      polygon_testnet: {
+        url: 'https://rpc-mumbai.maticvigil.com',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  }
 };
